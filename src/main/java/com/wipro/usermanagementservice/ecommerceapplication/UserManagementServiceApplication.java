c7eaa5429d20684c3c8eb4569b4bd5c3a1254d31
rahul revanth <rahul.revanth369@gmail.com>
2025-05-05 20:01:04 +0530

    changed file name to adjust directory

--- src/main/java/com/wipro/usermanagementservice/ecommerceapplication/UserManagementServiceApplication.java
+++ src/main/java/com/wipro/usermanagementservice/ecommerceapplication/UserManagementServiceApplication.java
@@ -1,4 +1,4 @@
-package com.wipro.usermanagementservice;
+package com.wipro.usermanagementservice.ecommerceapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
